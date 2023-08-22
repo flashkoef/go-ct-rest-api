73526147644ff24c07a515a75ade2b20720fa1dd
Alexander <a.bak@dotsource.de>
2023-08-22 17:37:14 +0200

    develop: added .env to gitignore

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
 type HttpServerConfig struct {
 	Port string
 }
@@ -11,11 +17,34 @@ type Config struct {
 func New() *Config {
 	return &Config{
 		HttpServer: HttpServerConfig{
-			Port: getEnv("", "8080"),
+			Port: getEnv("CI_HTTP_SERVER_PORT", "8080"),
 		},
 	}
 }
 
 func getEnv(key, defaultVal string) string {
+	if value, exists := getConfigParam(key); exists {
+		return value
+	}
+
 	return defaultVal
+}
+
+func getConfigParam(key string) (string, bool) {
+	readConfigFile()
+	
+	value, ok := viper.Get(key).(string)
+	if !ok {
+    log.Fatalf("Invalid type assertion while get a config parameter")
+  }
+	
+	return value, ok
+}
+
+func readConfigFile() {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
 }
