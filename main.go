d9e6e4b4a9c34d44671af8b85bc4dfb6a3f0329b
Alexander <a.bak@dotsource.de>
2023-08-22 14:14:42 +0200

    develop: implemented usage of router, controller and service

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 
+	c "github.com/flashkoef/go-ct-rest-api/controllers/v1"
+	s "github.com/flashkoef/go-ct-rest-api/services/v1"
+	r "github.com/flashkoef/go-ct-rest-api/routers/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,8 +15,8 @@ var router *gin.Engine
 func init() {
 	router = gin.New()
 	version1 := router.Group("/v1")
-	controller := ctrl.NewController(services.NewService())
-	v1.InitRoutes(version1, controller)
+	controller := c.NewHelloController(s.NewHelloService())
+	r.InitRoutes(version1, controller)
 }
 
 func main() {
