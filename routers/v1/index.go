1e2d3ab8afa539432fe6a7336e5c872ec6038f79
Alexander <a.bak@dotsource.de>
2023-08-22 14:08:54 +0200

    develop: implemented usage of controller

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -1,7 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/flashkoef/go-ct-rest-api/controllers/v1"
+	"github.com/gin-gonic/gin"
+)
 
-func InitRoutes(g *gin.RouterGroup, c *ctrl.Controller) {
+func InitRoutes(g *gin.RouterGroup, c *v1.Controller) {
 	SetHelloRoute(g, c)
 }
