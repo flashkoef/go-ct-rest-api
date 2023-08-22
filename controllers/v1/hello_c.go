0080d2c912effdd4c0a589044570833b28590925
Alexander <a.bak@dotsource.de>
2023-08-22 13:48:41 +0200

    develop: implemented hello-controller

---
+++ controllers/v1/hello_c.go
@@ -0,0 +1,15 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+type Controller struct {
+	serviceImpl services.Servicer
+}
+
+func NewHelloController(s *services.Service) *Controller {
+	return &Controller{serviceImpl: s}
+}
+
+func (c *Controller) HelloController(ctx *gin.Context) {
+	c.serviceImpl.HelloService()
+}
