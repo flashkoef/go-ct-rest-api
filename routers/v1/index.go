2862e7fc31b7fb79df8e601e45a7d7703eda5e99
Alexander <a.bak@dotsource.de>
2023-08-23 11:14:56 +0200

    develop: added function

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"github.com/flashkoef/go-ct-rest-api/controllers/v1/customers"
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
 	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
@@ -8,4 +9,5 @@ import (
 
 func InitRoutes(g *gin.RouterGroup) {	
 	SetHelloRoute(g, hello.NewHelloController(hs.NewHelloService()))
+	SetCustomersRoute(g, customers.New())
 }
