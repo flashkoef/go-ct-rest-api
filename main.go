343ad038ec9df706e5393208154dc6dfbc9a3e29
Alexander <a.bak@dotsource.de>
2023-08-23 10:57:11 +0200

    develop: refactored controller handling

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"net/http"
 
 	"github.com/flashkoef/go-ct-rest-api/config"
-	hc "github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
 	router "github.com/flashkoef/go-ct-rest-api/routers/v1"
-	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,8 +15,7 @@ var engine *gin.Engine
 func init() {
 	engine = gin.New()
 	routerGroup := engine.Group("/v1")
-	controller := hc.NewHelloController(hs.NewHelloService())
-	router.InitRoutes(routerGroup, controller)
+	router.InitRoutes(routerGroup)
 }
 
 func main() {
