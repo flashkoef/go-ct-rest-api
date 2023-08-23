dc9b6bdbdb440089527f55c94a9f3e9f793d0f80
Alexander <a.bak@dotsource.de>
2023-08-23 10:55:09 +0200

    develop: refactored controller handling

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -2,9 +2,10 @@ package v1
 
 import (
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
+	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
 )
 
-func InitRoutes(g *gin.RouterGroup, c *v1.Controller) {
-	SetHelloRoute(g, c)
+func InitRoutes(g *gin.RouterGroup) {	
+	SetHelloRoute(g, hello.NewHelloController(hs.NewHelloService()))
 }
