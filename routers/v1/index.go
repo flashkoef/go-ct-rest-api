9d87b6db813fb570ad6b1247903f9017428d99b7
Alexander <a.bak@dotsource.de>
2023-08-23 14:43:56 +0200

    develop: fixed parameter call

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -3,11 +3,12 @@ package v1
 import (
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/customers"
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
+	connector "github.com/flashkoef/go-ct-rest-api/libs"
 	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(g *gin.RouterGroup) {	
 	SetHelloRoute(g, hello.NewHelloController(hs.NewHelloService()))
-	SetCustomersRoute(g, customers.New())
+	SetCustomersRoute(g, customers.New(connector.New().GetProjectClient()))
 }
