bddbd113f7fc43ae1dbca0520131b959a5e4501a
Alexander <a.bak@dotsource.de>
2023-08-24 16:30:38 +0200

    develop: refactored due to move of logic in service

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -4,11 +4,12 @@ import (
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/customers"
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
+	"github.com/flashkoef/go-ct-rest-api/services"
 	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(g *gin.RouterGroup) {
 	SetHelloRoute(g, hello.NewHelloController(hs.NewHelloService()))
-	SetCustomersRoute(g, customers.New(connector.New().GetProjectClient()))
+	SetCustomersRoute(g, customers.New(services.New(connector.New().GetProjectClient())))
 }
