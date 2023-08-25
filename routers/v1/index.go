7eb51aa096f06392a361924e9af9d2bc3cc6c8c1
Alexander <a.bak@dotsource.de>
2023-08-25 12:52:07 +0200

    develop: updated package usage, renamed method, removed parameter

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -2,20 +2,17 @@ package v1
 
 import (
 	"github.com/flashkoef/go-ct-rest-api/controllers/v1"
-	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
 	"github.com/flashkoef/go-ct-rest-api/core/errors"
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
 	"github.com/flashkoef/go-ct-rest-api/services"
-	hs "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(rg *gin.RouterGroup) {
-	SetHelloRoute(rg, hello.NewHelloController(hs.NewHelloService()))
-	
-	SetCustomersRoute(rg, customers.New(
-			services.New(connector.New().GetProjectClient()),
-			errors.New(),
-		),
-	)
+	SetHelloRoute(rg, v1.NewHelloController())
+
+	SetCustomersRoute(rg, v1.NewCustomersController(
+		services.New(connector.New().GetProjectClient()),
+		errors.New(),
+	))
 }
