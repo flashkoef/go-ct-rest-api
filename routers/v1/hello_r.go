2aca3baeb557487753590a29d946dda247eb37f5
Alexander <a.bak@dotsource.de>
2023-08-25 12:50:55 +0200

    develop: updated package usage

--- routers/v1/hello_r.go
+++ routers/v1/hello_r.go
@@ -1,10 +1,10 @@
 package v1
 
 import (
-	"github.com/flashkoef/go-ct-rest-api/controllers/v1/hello"
+	"github.com/flashkoef/go-ct-rest-api/controllers/v1"
 	"github.com/gin-gonic/gin"
 )
 
-func SetHelloRoute(router *gin.RouterGroup, c *hello.Controller) {
+func SetHelloRoute(router *gin.RouterGroup, c *v1.HelloController) {
 	router.GET("/hello", c.Hello)
 }
