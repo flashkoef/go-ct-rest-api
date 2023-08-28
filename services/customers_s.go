4a495f04c0037e6dac8fc4d902b93369f5cf7662
Alexander <a.bak@dotsource.de>
2023-08-28 12:59:03 +0200

    develop: updated package name, refactored return values, implemented new service method

--- services/customers_s.go
+++ services/customers_s.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/flashkoef/go-ct-rest-api/core/errors"
+	"github.com/flashkoef/go-ct-rest-api/core/error_handler"
 	"github.com/gin-gonic/gin"
 	"github.com/labd/commercetools-go-sdk/platform"
 )
 
 type CustomersService struct {
 	projectClient *platform.ByProjectKeyRequestBuilder
+	errorHandler *error_handler.CheckError
 }
 
-func New(pc *platform.ByProjectKeyRequestBuilder) *CustomersService {
+func NewCustomerService(pc *platform.ByProjectKeyRequestBuilder, eh *error_handler.CheckError) *CustomersService {
 	return &CustomersService{
 		projectClient: pc,
+		errorHandler: eh,
 	}
 }
 
@@ -28,14 +30,25 @@ func (s *CustomersService) ExecuteGetCustomerByEmailRequest(ctx *gin.Context) (p
 	if err != nil {
 		msg := fmt.Sprintf("error while execute request to ctp %s", err)
 		log.Println(msg)
-		return platform.Customer{}, errors.NewInternalError(msg, err)
+		return platform.Customer{}, error_handler.NewInternalError(msg, err)
 	}
 
 	if len(customer.Results) == 0 {
 		msg := fmt.Sprintf("can't found customer with email %s", ctx.Query("email"))
 		log.Println(msg)
-		return platform.Customer{}, errors.NewNotFoundError(msg)
+		return platform.Customer{}, error_handler.NewNotFoundError(msg)
 	}
 
 	return customer.Results[0], nil
+}
+
+func (s *CustomersService) ExecuteGetCustomerByIdRequest(ctx *gin.Context) (*platform.Customer, error) {
+	customer, err := s.projectClient.Customers().WithId(ctx.Param("customerID")).Get().Execute(ctx)
+
+	shouldReturn, err := s.errorHandler.CheckCtSdkError(err, ctx)
+	if shouldReturn {
+		return &platform.Customer{}, err
+	}
+
+	return customer, nil
 }
