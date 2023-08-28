1a2d673a65eaa62d9bb33b9c6207689eb7cc4fb0
Alexander <a.bak@dotsource.de>
2023-08-28 13:01:26 +0200

    develop: renamed package, implemented new method

---
+++ core/error_handler/check_error.go
@@ -0,0 +1,45 @@
+package error_handler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/flashkoef/go-ct-rest-api/core/models"
+	"github.com/gin-gonic/gin"
+)
+
+type CheckError struct{}
+
+func New() *CheckError {
+	return &CheckError{}
+}
+
+func (ce *CheckError) CheckInternError(err error, ctx *gin.Context) bool {
+	if err != nil {
+		switch e := err.(type) {
+		case *NotFoundError:
+			ctx.JSON(http.StatusNotFound, models.NewErrorResponse(http.StatusNotFound, NotFoundErr, err))
+			return true
+		case *InternalError:
+			ctx.JSON(http.StatusInternalServerError, models.NewErrorResponse(http.StatusInternalServerError, InternalErr, err))
+			return true
+		default:
+			log.Printf("oops, this was unexpected: %s", e)
+			ctx.JSON(http.StatusInternalServerError, models.NewErrorResponse(http.StatusInternalServerError, InternalErr, err))
+			return true
+		}
+	}
+	return false
+}
+
+func (ce *CheckError) CheckCtSdkError(err error, ctx *gin.Context) (bool, error) {
+	if err != nil {
+		log.Printf("error while execute request to ctp %s", err)
+		if err.Error() == "resource not found" {
+			msg := fmt.Sprintf("can't found customer with id %s", ctx.Param("customerID"))
+			return true, NewNotFoundErrorWithOriginalErr(msg, err.Error())
+		}
+	}
+	return false, nil
+}
