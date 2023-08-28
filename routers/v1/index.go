577680b72b8d5bdc23ff717e45f856396c826a2c
Alexander <a.bak@dotsource.de>
2023-08-28 13:04:16 +0200

    develop: updated packages and parameters

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -1,8 +1,8 @@
 package v1
 
 import (
-	"github.com/flashkoef/go-ct-rest-api/controllers/v1"
-	"github.com/flashkoef/go-ct-rest-api/core/errors"
+	v1 "github.com/flashkoef/go-ct-rest-api/controllers/v1"
+	"github.com/flashkoef/go-ct-rest-api/core/error_handler"
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
 	"github.com/flashkoef/go-ct-rest-api/services"
 	"github.com/gin-gonic/gin"
@@ -12,7 +12,7 @@ func InitRoutes(rg *gin.RouterGroup) {
 	SetHelloRoute(rg, v1.NewHelloController())
 
 	SetCustomersRoute(rg, v1.NewCustomersController(
-		services.New(connector.New().GetProjectClient()),
-		errors.New(),
+		services.NewCustomerService(connector.New().GetProjectClient(), error_handler.New()),
+		error_handler.New(),
 	))
 }
