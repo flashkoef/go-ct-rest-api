276ec7d3e0ed67ea84fd4854a8f681e9d0bab587
Alexander <a.bak@dotsource.de>
2023-08-28 13:00:55 +0200

    develop: updated package name, used status from http-package, implemented new controller-method

--- controllers/v1/customers_c.go
+++ controllers/v1/customers_c.go
@@ -1,17 +1,19 @@
 package v1
 
 import (
-	"github.com/flashkoef/go-ct-rest-api/core/errors"
+	"net/http"
+
+	"github.com/flashkoef/go-ct-rest-api/core/error_handler"
 	"github.com/flashkoef/go-ct-rest-api/services"
 	"github.com/gin-gonic/gin"
 )
 
 type CustomersController struct {
 	customerService *services.CustomersService
-	checkError      *errors.CheckError
+	checkError      *error_handler.CheckError
 }
 
-func NewCustomersController(s *services.CustomersService, ce *errors.CheckError) *CustomersController {
+func NewCustomersController(s *services.CustomersService, ce *error_handler.CheckError) *CustomersController {
 	return &CustomersController{
 		customerService: s,
 		checkError:      ce,
@@ -21,10 +23,21 @@ func NewCustomersController(s *services.CustomersService, ce *errors.CheckError)
 func (c *CustomersController) GetCustomerByEmail(ctx *gin.Context) {
 	customer, err := c.customerService.ExecuteGetCustomerByEmailRequest(ctx)
 
-	shouldReturn := c.checkError.CheckError(err, ctx)
+	shouldReturn := c.checkError.CheckInternError(err, ctx)
 	if shouldReturn {
 		return
 	}
 
-	ctx.JSON(200, customer)
+	ctx.JSON(http.StatusOK, customer)
+}
+
+func (c *CustomersController) GetCustomerByID(ctx *gin.Context) {
+	customer, err := c.customerService.ExecuteGetCustomerByIdRequest(ctx)
+
+	shouldReturn := c.checkError.CheckInternError(err, ctx)
+	if shouldReturn {
+		return
+	}
+
+	ctx.JSON(http.StatusOK, customer)
 }
