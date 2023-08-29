805d6de0141a486e75c12eb0e203dc13308a33b2
Alexander <a.bak@dotsource.de>
2023-08-29 16:51:30 +0200

    develop: implemented route to init

--- routers/v1/index.go
+++ routers/v1/index.go
@@ -3,6 +3,7 @@ package v1
 import (
 	controllers "github.com/flashkoef/go-ct-rest-api/controllers/v1"
 	"github.com/flashkoef/go-ct-rest-api/core/error_handler"
+	"github.com/flashkoef/go-ct-rest-api/core/mapper"
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
 	services "github.com/flashkoef/go-ct-rest-api/services/v1"
 	"github.com/gin-gonic/gin"
@@ -11,8 +12,14 @@ import (
 func InitRoutes(rg *gin.RouterGroup) {
 	SetHelloRoute(rg, controllers.NewHelloController())
 
-	SetCustomersRoute(rg, controllers.NewCustomersController(
+	SetCustomerRoute(rg, controllers.NewCustomersController(
 		services.NewCustomerService(connector.New().GetProjectClient(), error_handler.New()),
 		error_handler.New(),
+	))
+
+	SetProductRoute(rg, controllers.NewProductController(
+		services.NewProductService(connector.New().GetProjectClient(), error_handler.New()),
+		error_handler.New(),
+		mapper.NewProductMapper(),
 	))
 }
