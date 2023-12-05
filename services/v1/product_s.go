c2f9f4e0f2c2eca6d3a1e57bfdc9d677aa86c607
Alexander <a.bak@dotsource.de>
2023-12-05 13:09:26 +0100

    develop: adjusted error handling

--- services/v1/product_s.go
+++ services/v1/product_s.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/flashkoef/go-ct-rest-api/core/error_handler"
 	"github.com/gin-gonic/gin"
@@ -31,25 +30,10 @@ func (s *ProductService) GetProductBySlug(ctx *gin.Context) (platform.ProductPro
 		Where([]string{fmt.Sprintf("slug(%s=\"%s\")", ctx.Query("language"), ctx.Query("slug"))}).
 		Execute(ctx)
 	
-	shouldReturn, err := checkError(ctSdkErr, result, ctx)
+	shouldReturn, err := s.errorHandler.CheckCtSdkErrorForPagedResponse(ctSdkErr, result, ctx)
 	if shouldReturn {
 		return platform.ProductProjection{}, err
 	}
 
 	return result.Results[0], nil
-}
-
-func checkError(err error, result *platform.ProductProjectionPagedQueryResponse, ctx *gin.Context) (bool, error) {
-	if err != nil {
-		msg := fmt.Sprintf("error while execute request to ctp %s", err)
-		log.Println(msg)
-		return true, error_handler.NewInternalError(msg, err)
-	}
-
-	if len(result.Results) == 0 {
-		msg := fmt.Sprintf("can't found product projection with slug %s", ctx.Query("slug"))
-		log.Println(msg)
-		return true, error_handler.NewNotFoundError(msg)
-	}
-	return false, nil
 }
