7c76fdbab8b2aabba9e7275a02600067110f5ad4
Alexander <a.bak@dotsource.de>
2023-12-05 13:08:11 +0100

    develop: added and adjusted error handling

--- services/v1/customer_s.go
+++ services/v1/customer_s.go
@@ -27,30 +27,38 @@ func NewCustomerService(pc *platform.ByProjectKeyRequestBuilder, eh error_handle
 }
 
 func (s *CustomerService) ExecuteGetCustomerByEmailRequest(ctx *gin.Context) (platform.Customer, error) {
-	customer, err := s.projectClient.Customers().
+	result, ctSdkErr := s.projectClient.Customers().
 		Get().
 		Where([]string{fmt.Sprintf("email=\"%s\"", ctx.Query("email"))}).
 		Execute(ctx)
 
+	shouldReturn, err := checkError(ctSdkErr, result, ctx)
+	if shouldReturn {
+		return platform.Customer{}, err
+	}
+
+	return result.Results[0], nil
+}
+
+func checkError(err error, result *platform.CustomerPagedQueryResponse, ctx *gin.Context) (bool, error) {
 	if err != nil {
 		msg := fmt.Sprintf("error while execute request to ctp %s", err)
 		log.Println(msg)
-		return platform.Customer{}, error_handler.NewInternalError(msg, err)
+		return true, error_handler.NewInternalError(msg, err)
 	}
 
-	if len(customer.Results) == 0 {
+	if len(result.Results) == 0 {
 		msg := fmt.Sprintf("can't found customer with email %s", ctx.Query("email"))
 		log.Println(msg)
-		return platform.Customer{}, error_handler.NewNotFoundError(msg)
+		return true, error_handler.NewNotFoundError(msg)
 	}
-
-	return customer.Results[0], nil
+	return false, nil
 }
 
 func (s *CustomerService) ExecuteGetCustomerByIdRequest(ctx *gin.Context) (*platform.Customer, error) {
 	customer, err := s.projectClient.Customers().WithId(ctx.Param("customerID")).Get().Execute(ctx)
 
-	shouldReturn, err := s.errorHandler.CheckCtSdkError(err, ctx)
+	shouldReturn, err := s.errorHandler.CheckCtSdkErrorForNonPagedResponse(err, ctx)
 	if shouldReturn {
 		return &platform.Customer{}, err
 	}
