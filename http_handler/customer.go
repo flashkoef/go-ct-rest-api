4f98c26adf0c88e0504a9a4e9110bbc0d874fc15
Alexander <a.bak@dotsource.de>
2023-12-08 14:22:09 +0100

    develop: injected mapper to handler, implemented error handling

--- http_handler/customer.go
+++ http_handler/customer.go
@@ -1,9 +1,12 @@
 package http_handler
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/flashkoef/go-ct-rest-api/error_handler"
+	"github.com/flashkoef/go-ct-rest-api/mapper"
 	"github.com/flashkoef/go-ct-rest-api/service"
 	"github.com/gin-gonic/gin"
 )
@@ -11,32 +14,46 @@ import (
 type CustomerHandler struct {
 	customerService service.CustomerServicer
 	checkError      error_handler.ErrorHandler
+	mapper mapper.CustomerMapper
 }
 
 func NewCustomersHandler(
 	customerService service.CustomerServicer,
 	checkError error_handler.ErrorHandler,
+	mapper mapper.CustomerMapper,
 ) *CustomerHandler {
 	return &CustomerHandler{
 		customerService: customerService,
 		checkError:      checkError,
+		mapper: mapper,
 	}
 }
 
 func (handler *CustomerHandler) GetCustomerByEmail(ctx *gin.Context) {
-	customer, err := handler.customerService.GetCustomerByEmail(ctx)
-
-	shouldReturn := handler.checkError.CheckInternError(err, ctx)
-	if shouldReturn {
+	result, err := handler.customerService.GetCustomerByEmail(ctx)
+	
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
 		return
 	}
+
+	if len(result.Results) == 0 {
+		msg := fmt.Sprintf("Can't found customer with email %s", ctx.Param("email"))
+		log.Println(msg)
+		err := error_handler.NewNotFoundError(msg)
+		if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+			return
+		}
+	}
+
+	customer, _ := handler.mapper.MapCtCustomerToCustomer(result.Results[0])
 
 	ctx.JSON(http.StatusOK, customer)
 }
 
+
 func (handler *CustomerHandler) GetCustomerByID(ctx *gin.Context) {
 	customer, err := handler.customerService.GetCustomerById(ctx)
-
+	
 	shouldReturn := handler.checkError.CheckInternError(err, ctx)
 	if shouldReturn {
 		return
