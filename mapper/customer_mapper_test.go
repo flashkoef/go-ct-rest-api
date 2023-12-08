db3b884748050216b3cbebfdb4ae40d6ce19ae21
Alexander <a.bak@dotsource.de>
2023-12-08 14:19:34 +0100

    develop: added skips for returned err value in test success cases

--- mapper/customer_mapper_test.go
+++ mapper/customer_mapper_test.go
@@ -8,24 +8,24 @@ import (
 
 func TestMapCtCustomerToCustomer(t *testing.T) {
 	m := NewMapper()
-	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomer)
+	mappedCustomer, _ := m.MapCtCustomerToCustomer(ctCustomer)
 	assert.Equal(t, expCustomer, mappedCustomer)
 }
 
 func TestMapCtCustomerToCustomerWithEmptyAddresses(t *testing.T) {
 	m := NewMapper()
-	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddresses)
+	mappedCustomer, _ := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddresses)
 	assert.Equal(t, expCustomerWithEmptyAddresses, mappedCustomer)
 }
 
 func TestCtCustomerToCustomerWithEmptyCustomerFields(t *testing.T) {
 	m := NewMapper()
-	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyFields)
+	mappedCustomer, _ := m.MapCtCustomerToCustomer(ctCustomerWithEmptyFields)
 	assert.Equal(t, expCustomerWithEmptyFields, mappedCustomer)
 }
 
 func TestMapCtCustomerToCustomerWithEmptyAddressFields(t *testing.T) {
 	m := NewMapper()
-	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddressFields)
+	mappedCustomer, _ := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddressFields)
 	assert.Equal(t, expCustomerWithEmptyAddressFields, mappedCustomer)
 }
