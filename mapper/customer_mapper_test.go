80f6addf49d61810b624f3f09c301481b7b587a4
Alexander <a.bak@dotsource.de>
2023-12-08 11:10:11 +0100

    develop: implemented unit tests

---
+++ mapper/customer_mapper_test.go
@@ -0,0 +1,31 @@
+package mapper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMapCtCustomerToCustomer(t *testing.T) {
+	m := NewMapper()
+	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomer)
+	assert.Equal(t, expCustomer, mappedCustomer)
+}
+
+func TestMapCtCustomerToCustomerWithEmptyAddresses(t *testing.T) {
+	m := NewMapper()
+	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddresses)
+	assert.Equal(t, expCustomerWithEmptyAddresses, mappedCustomer)
+}
+
+func TestCtCustomerToCustomerWithEmptyCustomerFields(t *testing.T) {
+	m := NewMapper()
+	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyFields)
+	assert.Equal(t, expCustomerWithEmptyFields, mappedCustomer)
+}
+
+func TestMapCtCustomerToCustomerWithEmptyAddressFields(t *testing.T) {
+	m := NewMapper()
+	mappedCustomer := m.MapCtCustomerToCustomer(ctCustomerWithEmptyAddressFields)
+	assert.Equal(t, expCustomerWithEmptyAddressFields, mappedCustomer)
+}
