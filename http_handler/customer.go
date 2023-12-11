d2a4496241b67d22c107b6d291c1669ec5bccb91
Alexander <a.bak@dotsource.de>
2023-12-11 13:54:23 +0100

    develop: moved get customer by email handler to its own file

--- http_handler/customer.go
+++ http_handler/customer.go
@@ -1,8 +1,6 @@
 package http_handler
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/flashkoef/go-ct-rest-api/error_handler"
@@ -27,30 +25,6 @@ func NewCustomersHandler(
 		checkError:      checkError,
 		mapper:          mapper,
 	}
-}
-
-func (handler *CustomerHandler) GetCustomerByEmail(ctx *gin.Context) {
-	result, err := handler.customerService.GetCustomerByEmail(ctx)
-
-	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
-		return
-	}
-
-	if len(result.Results) == 0 {
-		msg := fmt.Sprintf("Can't found customer with email %s", ctx.Param("email"))
-		log.Println(msg)
-		err := error_handler.NewNotFoundError(msg)
-		if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
-			return
-		}
-	}
-
-	customer, err := handler.mapper.MapCtCustomerToCustomer(result.Results[0])
-	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
-		return
-	}
-
-	ctx.JSON(http.StatusOK, customer)
 }
 
 func (handler *CustomerHandler) GetCustomerByID(ctx *gin.Context) {
