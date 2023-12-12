4341713a0ca3e92b9087bf63433f79aa9095a9ae
Alexander <a.bak@dotsource.de>
2023-12-12 16:54:58 +0100

    develop: added Password field

--- model/customer.go
+++ model/customer.go
@@ -2,6 +2,7 @@ package model
 
 type Customer struct {
 	Email       string    `json:"email"`
+	Password    string    `json:"password"`
 	FirstName   string    `json:"firstName,omitempty"`
 	LastName    string    `json:"lastName,omitempty"`
 	DateOfBirth string    `json:"dateOfBirth,omitempty"`
