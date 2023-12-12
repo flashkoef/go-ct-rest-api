b5990acdbe1889298c29b3ba04781131f499c063
Alexander <a.bak@dotsource.de>
2023-12-12 16:47:35 +0100

    develop: implemented handler method to upsert customer

---
+++ http_handler/upsert_customer.go
@@ -0,0 +1,44 @@
+package http_handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (handler *CustomerHandler) UpsertCustomer(ctx *gin.Context) {
+	customerPagedQueryResponse, err := handler.customerService.GetCustomerByEmail(ctx)
+
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	if len(customerPagedQueryResponse.Results) == 0 {
+		customerSignInResult, err := handler.customerService.CreateCustomer(ctx)
+		log.Printf("Created customer in ctp, with ID: %s", customerSignInResult.Customer.ID)
+		if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+			return
+		}
+
+		customer, err := handler.mapper.MapCtCustomerToCustomer(customerSignInResult.Customer)
+		if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, customer)
+		return
+	}
+
+	ctCustomer, err := handler.customerService.UpdateCustomer(customerPagedQueryResponse.Results[0], ctx)
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	customer, err := handler.mapper.MapCtCustomerToCustomer(*ctCustomer)
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	ctx.JSON(http.StatusOK, customer)
+}
