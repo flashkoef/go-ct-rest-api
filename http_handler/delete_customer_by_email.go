372a1617faa8cea3fdee5ecdfefbac69b57135b6
Alexander <a.bak@dotsource.de>
2023-12-12 16:47:58 +0100

    develop: implemented handler method to delete customer

---
+++ http_handler/delete_customer_by_email.go
@@ -0,0 +1,42 @@
+package http_handler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/flashkoef/go-ct-rest-api/error_handler"
+	"github.com/gin-gonic/gin"
+)
+
+func (handler *CustomerHandler) DeleteCustomerByEmail(ctx *gin.Context) {
+	customerPagedQueryResponse, err := handler.customerService.GetCustomerByEmail(ctx)
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	if len(customerPagedQueryResponse.Results) == 0 {
+		msg := fmt.Sprintf("Can't found customer with email %s", ctx.Param("email"))
+		log.Println(msg)
+		err := error_handler.NewNotFoundError(msg)
+		if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+			return
+		}
+	}
+
+	ctCustomer, err := handler.customerService.DeleteCustomerByID(
+		customerPagedQueryResponse.Results[0].ID,
+		customerPagedQueryResponse.Results[0].Version,
+		ctx,
+	)
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	customer, err := handler.mapper.MapCtCustomerToCustomer(*ctCustomer)
+	if shouldReturn := handler.checkError.CheckInternError(err, ctx); shouldReturn {
+		return
+	}
+
+	ctx.JSON(http.StatusOK, customer)
+}
