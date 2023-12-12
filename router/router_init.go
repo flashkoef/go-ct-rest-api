4c6c1037205bc6c32a5e6c297b1635d85ad9cb29
Alexander <a.bak@dotsource.de>
2023-12-12 18:19:52 +0100

    develop: added parameter

--- router/router_init.go
+++ router/router_init.go
@@ -6,13 +6,16 @@ import (
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
 	"github.com/flashkoef/go-ct-rest-api/mapper"
 	"github.com/flashkoef/go-ct-rest-api/service"
+	"github.com/flashkoef/go-ct-rest-api/update"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(routerGroup *gin.RouterGroup) {
-	SetCustomerRoute(routerGroup, http_handler.NewCustomersHandler(
-		service.NewCustomerService(connector.New().GetProjectClient(), error_handler.New()),
-		error_handler.New(),
-		mapper.NewMapper(),
-	))
+	SetCustomerRoute(
+		routerGroup, http_handler.NewCustomersHandler(
+			service.NewCustomerService(connector.New().GetProjectClient(), error_handler.New(), update.NewCtCustomerUpdate()),
+			error_handler.New(),
+			mapper.NewMapper(),
+		),
+	)
 }
