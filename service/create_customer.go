9e82406865a81e6492e807f5f1786018314c1e20
Alexander <a.bak@dotsource.de>
2023-12-12 16:46:10 +0100

    develop: implemented create customer service method

---
+++ service/create_customer.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/flashkoef/go-ct-rest-api/error_handler"
+	"github.com/gin-gonic/gin"
+	"github.com/labd/commercetools-go-sdk/platform"
+)
+
+func (service *CustomerService) CreateCustomer(ctx *gin.Context) (*platform.CustomerSignInResult, error) {
+	var customerDraft platform.CustomerDraft
+	if err := ctx.BindJSON(&customerDraft); err != nil {
+		msg := fmt.Sprintln("Error while created customer draft.")
+		log.Println(msg)
+		return &platform.CustomerSignInResult{}, error_handler.NewInternalError(msg, err)
+	}
+
+	customerSignInResult, err := service.ctClient.Customers().Post(customerDraft).Execute(ctx)
+	if err != nil {
+		msg := fmt.Sprintln("Error while execute create customer request to commercetools platform.")
+		log.Println(msg)
+		return &platform.CustomerSignInResult{}, error_handler.NewCtpError(msg, err)
+	}
+
+	return customerSignInResult, nil
+}
