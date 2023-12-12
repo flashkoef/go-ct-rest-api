ed10f5dc2646c76376d979ef70b3f7e325c3ad92
Alexander <a.bak@dotsource.de>
2023-12-12 16:45:11 +0100

    develop: implemented delete customer service

---
+++ service/delete_customer.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/flashkoef/go-ct-rest-api/error_handler"
+	"github.com/gin-gonic/gin"
+	"github.com/labd/commercetools-go-sdk/platform"
+)
+
+func (service *CustomerService) DeleteCustomerByID(customerID string, version int, ctx *gin.Context) (*platform.Customer, error) {
+	ctCustomer, err := service.ctClient.Customers().WithId(customerID).Delete().Version(version).Execute(ctx)
+	if err != nil {
+		msg := fmt.Sprintln("Error while execute delete customer request to commercetools platform.")
+		log.Println(msg)
+		return &platform.Customer{}, error_handler.NewCtpError(msg, err)
+	}
+
+	return ctCustomer, nil
+}
