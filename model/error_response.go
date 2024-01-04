45610fb191dfa9f77200b32ba4fb05a0e8d30be0
Alexander <a.bak@dotsource.de>
2024-01-04 17:47:31 +0100

    develop: changed type

--- model/error_response.go
+++ model/error_response.go
@@ -3,13 +3,13 @@ package model
 type ErrorResponse struct {
 	Message string `json:"message"`
 	Code    string `json:"code"`
-	Error   error  `json:"error"`
+	Error   string  `json:"error"`
 }
 
 func NewErrorResponse(msg string, code string, err error) *ErrorResponse {
 	return &ErrorResponse{
 		Message: msg,
 		Code:    code,
-		Error:   err,
+		Error:   err.Error(),
 	}
 }
