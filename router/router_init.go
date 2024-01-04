07025b1754735d861d1bd7d123cfb8873abab3fb
Alexander <a.bak@dotsource.de>
2024-01-04 10:31:24 +0100

    develop: removed field due to moved logic to service

--- router/router_init.go
+++ router/router_init.go
@@ -6,14 +6,13 @@ import (
 	"github.com/flashkoef/go-ct-rest-api/libs/commercetools/connector"
 	"github.com/flashkoef/go-ct-rest-api/mapper"
 	"github.com/flashkoef/go-ct-rest-api/service"
-	"github.com/flashkoef/go-ct-rest-api/update"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(routerGroup *gin.RouterGroup) {
 	SetCustomerRoute(
 		routerGroup, http_handler.NewCustomersHandler(
-			service.NewCustomerService(connector.New().GetProjectClient(), error_handler.New(), update.NewCtCustomerUpdate()),
+			service.NewCustomerService(connector.New().GetProjectClient(), error_handler.New()),
 			error_handler.New(),
 			mapper.NewMapper(),
 		),
