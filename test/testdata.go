30aa8e3fb9d1f62e42e5517a15520a44b7909745
Alexander <a.bak@dotsource.de>
2024-01-04 17:48:14 +0100

    develop: added test data

---
+++ test/testdata.go
@@ -0,0 +1,27 @@
+package test
+
+import "github.com/flashkoef/go-ct-rest-api/model"
+
+var (
+	expectedCustomer = model.Customer{
+		Email: "jane.doe@test.de",
+		Password: "****wRs=",
+		FirstName: "Jane",
+		LastName: "Doe",
+		DateOfBirth: "2019-09-07",
+		Addresses: []model.Address{
+			{
+				StreetName: "Musterstraße 12",
+				City: "Musterstadt",
+				PostalCode: "01234",
+				County: "DE",
+			},
+		},
+	}
+
+ 	expectedErrorResponse = model.ErrorResponse{
+		Message:"Resource not found.", 
+		Code:"NOT_FOUND", 
+		Error:"NotFoundError: Can't found customer with email unknown@test.de",
+	}
+)
