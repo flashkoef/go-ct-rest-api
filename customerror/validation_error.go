c25987d60dbab94654e9d95c06fbcebd2819cb6c
Alexander <a.bak@dotsource.de>
2024-01-12 14:17:32 +0100

    develop: implemented validation error

---
+++ customerror/validation_error.go
@@ -0,0 +1,23 @@
+package customerror
+
+import "fmt"
+
+const ValidationErr = "ValidationError"
+
+type ValidationError struct {
+	ErrCode string `json:"errCode"`
+	Message string `json:"message"`
+	Err     error  `json:"err"`
+}
+
+func NewValidationError(msg string, err error) *ValidationError {
+	return &ValidationError{
+		ErrCode: InternalErr,
+		Message: msg,
+		Err:     err,
+	}
+}
+
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("%s, error: %s", e.Message, e.Err)
+}
