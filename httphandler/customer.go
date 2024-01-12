5659cda8b08d2dee77402e7ef8975944fe7223f8
Alexander <a.bak@dotsource.de>
2024-01-12 14:18:28 +0100

    develop: injected validator customer handler

--- httphandler/customer.go
+++ httphandler/customer.go
@@ -4,22 +4,26 @@ import (
 	"github.com/flashkoef/go-ct-rest-api/errorhandler"
 	"github.com/flashkoef/go-ct-rest-api/mapper"
 	"github.com/flashkoef/go-ct-rest-api/service"
+	"github.com/flashkoef/go-ct-rest-api/validator"
 )
 
 type CustomerHandler struct {
 	customerService service.CustomerServicer
 	errorHandler    errorhandler.ErrorHandler
 	mapper          mapper.CustomerMapper
+	validator       validator.Validator
 }
 
 func NewCustomersHandler(
 	customerService service.CustomerServicer,
 	errorHandler errorhandler.ErrorHandler,
 	mapper mapper.CustomerMapper,
+	validator validator.Validator,
 ) *CustomerHandler {
 	return &CustomerHandler{
 		customerService: customerService,
 		errorHandler:    errorHandler,
 		mapper:          mapper,
+		validator:       validator,
 	}
 }
