de70983d6e0da62d89b4837065a109bac88391de
Alexander <a.bak@dotsource.de>
2024-01-19 15:20:40 +0100

    develop: implemented validation for the provided customer

--- httphandler/upsert_customer.go
+++ httphandler/upsert_customer.go
@@ -4,10 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/flashkoef/go-ct-rest-api/customerror"
+	"github.com/flashkoef/go-ct-rest-api/model"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 func (handler *CustomerHandler) UpsertCustomer(ctx *gin.Context) {
+	var customerToBeValidated model.Customer
+
+	err := ctx.ShouldBindBodyWith(&customerToBeValidated, binding.JSON)
+	if err != nil {
+		e := customerror.NewValidationError("validation for provided customer failed", err)
+		if shouldReturn := handler.errorHandler.CheckError(e, ctx); shouldReturn {
+			return
+		}
+	}
+
 	customerPagedQueryResponse, err := handler.customerService.GetCustomerByEmail(ctx)
 
 	if shouldReturn := handler.errorHandler.CheckError(err, ctx); shouldReturn {
